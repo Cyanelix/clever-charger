c7605fc644f7bf639d0dc01c09af9810f55d74dc
Dave Payne <d@vepayne.com>
2021-01-05 15:04:42 +0000

    Add scheduling and logging

--- src/main/java/com/cyanelix/chargetimer/charges/ChargeScheduler.java
+++ src/main/java/com/cyanelix/chargetimer/charges/ChargeScheduler.java
@@ -5,13 +5,18 @@ import com.cyanelix.chargetimer.electricity.RatePeriod;
 import com.cyanelix.chargetimer.microtypes.RequiredCharge;
 import com.cyanelix.chargetimer.tesla.TeslaClient;
 import com.cyanelix.chargetimer.tesla.model.ChargeState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 
 @Component
 public class ChargeScheduler {
+    private static final Logger LOG = LoggerFactory.getLogger(ChargeScheduler.class);
+
     private final TeslaClient teslaClient;
     private final ChargeStateService chargeStateService;
     private final RequiredChargesRepository requiredChargesRepository;
@@ -27,28 +32,38 @@ public class ChargeScheduler {
         this.clock = clock;
     }
 
-    // TODO: need to schedule this
+    @Scheduled(fixedRate = 120000L)
     public void chargeIfNeeded() {
+        LOG.debug("Checking...");
+
         ChargeState chargeState = chargeStateService.getChargeState();
         if (chargeState.isUnplugged() || chargeState.isFullyCharged()) {
             // Car's unplugged or full; nothing we can do for now.
+            LOG.debug("Nothing to do, charge state: {}", chargeState);
             return;
         }
 
         RequiredCharge nextRequiredCharge = requiredChargesRepository.getNextRequiredCharge();
         if (nextRequiredCharge == null) {
             // We don't have any required charges; do nothing and check again in a while.
+            LOG.debug("Nothing to do, no required charges");
             return;
         }
+
+        LOG.debug("Next required charge: {}", nextRequiredCharge);
 
         RatePeriod nextChargePeriod = chargeCalculator.getNextChargePeriod(
                 nextRequiredCharge, chargeState.getChargeLevel());
 
+        LOG.debug("Next charge period: {}", nextChargePeriod);
+
         // TODO: Might need to wake up the car in either of these cases.
         if (nextChargePeriod.chargeNow(clock) && chargeState.isReadyToCharge()) {
+            LOG.debug("Starting charging");
             teslaClient.setChargeLimit(nextRequiredCharge.getChargeLevel());
             teslaClient.startCharging();
         } else if (!nextChargePeriod.chargeNow(clock) && chargeState.isCharging()) {
+            LOG.debug("Stopping charging");
             teslaClient.stopCharging();
         }
     }
