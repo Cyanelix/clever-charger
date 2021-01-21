5d89eca9ac2bc4bb77403552d051282e3b0f6e0d
Dave Payne <d@vepayne.com>
2021-01-21 22:35:55 +0000

    Fix bug where past charging periods are returned
    - Previously the FutureRates class for the Agile tariff were returning all known periods, which included those from the past.

--- src/main/java/com/cyanelix/chargetimer/octopus/FutureRates.java
+++ src/main/java/com/cyanelix/chargetimer/octopus/FutureRates.java
@@ -7,19 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Clock;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class FutureRates {
     private static final Logger LOG = LoggerFactory.getLogger(FutureRates.class);
 
     private final OctopusClient octopusClient;
+    private final Clock clock;
 
     private List<UnitRate> unitRates;
 
     @Autowired
-    public FutureRates(OctopusClient octopusClient) {
+    public FutureRates(OctopusClient octopusClient, Clock clock) {
         this.octopusClient = octopusClient;
+        this.clock = clock;
     }
 
     @Scheduled(cron = "0 30 * * * ?")
@@ -30,9 +35,14 @@ public class FutureRates {
     }
 
     public List<UnitRate> getUnitRatesFromNow() {
+        ZonedDateTime now = ZonedDateTime.now(clock);
+
         if (unitRates == null) {
             cacheUnitRates();
         }
-        return unitRates;
+
+        return unitRates.stream()
+                .filter(unitRate -> unitRate.getValidTo().isAfter(now))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/cyanelix/chargetimer/octopus/FutureRatesTest.java
+++ src/test/java/com/cyanelix/chargetimer/octopus/FutureRatesTest.java
@@ -2,14 +2,18 @@ package com.cyanelix.chargetimer.octopus;
 
 import com.cyanelix.chargetimer.octopus.model.UnitRate;
 import com.cyanelix.chargetimer.octopus.model.UnitRatesResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,11 +23,17 @@ import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class FutureRatesTest {
+    private final Clock clock = Clock.fixed(Instant.parse("2021-01-01T12:15:00Z"), ZoneId.of("UTC"));
+
     @Mock
     private OctopusClient octopusClient;
 
-    @InjectMocks
     private FutureRates futureRates;
+
+    @BeforeEach
+    void beforeEach() {
+        futureRates = new FutureRates(octopusClient, clock);
+    }
 
     @Test
     void nullUnitRates_get_callsCacheAgain() {
@@ -42,5 +52,48 @@ class FutureRatesTest {
 
         // Then...
         assertThat(unitRatesFromNow).isNotEmpty();
+    }
+
+    @Test
+    void unitRatesFromPastToFuture_get_returnsOnlyCurrentOrFutureRates() {
+        // Given...
+        List<UnitRate> unitRates = Arrays.asList(
+                new UnitRate(BigDecimal.ONE, ZonedDateTime.parse("2021-01-01T11:30Z"),
+                        ZonedDateTime.parse("2021-01-01T12:00Z")),
+                new UnitRate(BigDecimal.valueOf(2), ZonedDateTime.parse("2021-01-01T12:00Z"),
+                        ZonedDateTime.parse("2021-01-01T12:30Z")),
+                new UnitRate(BigDecimal.valueOf(3), ZonedDateTime.parse("2021-01-01T12:30Z"),
+                        ZonedDateTime.parse("2021-01-01T13:00Z")));
+
+        UnitRatesResponse unitRatesResponse = mock(UnitRatesResponse.class);
+        given(unitRatesResponse.getResults()).willReturn(unitRates);
+
+        given(octopusClient.getRatesFromNow()).willReturn(unitRatesResponse);
+
+        // When...
+        List<UnitRate> unitRatesFromNow = futureRates.getUnitRatesFromNow();
+
+        // Then...
+        assertThat(unitRatesFromNow).extracting("valueIncVat")
+                .containsExactly(BigDecimal.valueOf(2), BigDecimal.valueOf(3));
+    }
+
+    @Test
+    void unitRatesOnlyInPast_get_returnEmptyList() {
+        // Given...
+        List<UnitRate> unitRates = Collections.singletonList(
+                new UnitRate(BigDecimal.ONE, ZonedDateTime.parse("2021-01-01T11:30Z"),
+                        ZonedDateTime.parse("2021-01-01T12:00Z")));
+
+        UnitRatesResponse unitRatesResponse = mock(UnitRatesResponse.class);
+        given(unitRatesResponse.getResults()).willReturn(unitRates);
+
+        given(octopusClient.getRatesFromNow()).willReturn(unitRatesResponse);
+
+        // When...
+        List<UnitRate> unitRatesFromNow = futureRates.getUnitRatesFromNow();
+
+        // Then...
+        assertThat(unitRatesFromNow).isEmpty();
     }
 }
